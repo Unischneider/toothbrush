9414f413519ade4a5d0d03589bd79c65921da434
Nick Schneider <n.schneider@tum.de>
2018-02-19 17:30:50 +0100

    resolved

--- app/controllers/toothbrushes_controller.rb
+++ app/controllers/toothbrushes_controller.rb
@@ -1,10 +1,10 @@
 class ToothbrushesController < ApplicationController
-<<<<<<< HEAD
+
   def show
     @toothbrush = Toothbrush.find(params[:id])
-=======
+
   def index
     @toothbtushes = Toothbrush.all
->>>>>>> master
+
   end
 end
