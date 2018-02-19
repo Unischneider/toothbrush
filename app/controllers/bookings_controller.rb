b7de72284db6f86ce14a2fb42f189509b461d951
Eric Berner <john-eric.berner@t-online.de>
2018-02-19 17:31:30 +0100

    Booking_new_first_version

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,14 @@
 class BookingsController < ApplicationController
+
+  def index
+    @toothbrushes = Toothbrush.all
+  end
+
+  def show
+    @toothbrush = Toothbrush.find(params[:id])
+  end
+
+  def new
+    @toothbrush = Toothbrush.new
+  end
 end
