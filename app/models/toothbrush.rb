4c94d191ea06923cecd7a130aa239612a33c6f43
Juliette Chevalier <juliettech.1393@gmail.com>
2018-02-20 16:08:57 +0100

    changes to .signed_in? method to work and uncommented cloudinary else shit breaks

--- app/models/toothbrush.rb
+++ app/models/toothbrush.rb
@@ -2,5 +2,5 @@ class Toothbrush < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :bookings
   validates :user_id, presence: true
-  #mount_uploader :photo, PhotoUploader
+  mount_uploader :photo, PhotoUploader
 end
