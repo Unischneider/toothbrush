8235c10de03cea392d2eb726ca2532e9aad997d3
Nick Schneider <n.schneider@tum.de>
2018-02-21 15:19:41 +0100

    adding about rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :toothbrushes
   resources :bookings, only: [:new, :show, :index]
   resources :users, only: [:show, :edit, :update]
