408609a2394a58b59b0115985c9a33ddc0e4e505
Juliette Chevalier <juliettech.1393@gmail.com>
2018-02-21 14:29:26 +0100

    user's controller left out in master? strange..

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def show
+    @user = current_user
+    authorize @user
+  end
+
+  def edit
+    @user = current_user
+    authorize @user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(set_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+    authorize @user
+  end
+
+  private
+
+  def set_params
+    params.require(:user).permit(:first_name, :last_name, :address, :gender)
+  end
+end
